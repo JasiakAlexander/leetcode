53d3a89d991c7d1447e27f205197daaef874a5c8
JasiakAlexander <alexanderjasiak1@gmail.com>
2023-03-04 14:11:05 +0100

    Add two_sum tests

--- test_two_sum.py
+++ test_two_sum.py
@@ -5,3 +5,6 @@ def test_two_sum():
     assert two_sum.naive_two_sum([2, 7, 11, 15], 9) == [0, 1]
     assert two_sum.naive_two_sum([3, 2, 4], 6) == [1, 2]
     assert two_sum.naive_two_sum([3, 3], 6) == [0, 1]
+    assert two_sum.smart_two_sum([2, 7, 11, 15], 9) == [0, 1]
+    assert two_sum.smart_two_sum([3, 2, 4], 6) == [1, 2]
+    assert two_sum.smart_two_sum([3, 3], 6) == [0, 1]
