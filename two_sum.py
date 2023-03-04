1a8668172c8907fcea6ff73e3e4f0fca7f8a8b55
JasiakAlexander <alexanderjasiak1@gmail.com>
2023-03-04 14:10:58 +0100

    Add two_sum tests

--- two_sum.py
+++ two_sum.py
@@ -7,3 +7,9 @@ def naive_two_sum(nums, target):
             num_j = nums[j]
             if num_j == sub_target:
                 return [i, j]
+def smart_two_sum(nums, target):
+    d = {}
+    for i in range(len(nums)):
+        if target - nums[i] in d:
+            return [d[target - nums[i]], i]
+        d[nums[i]] = i
